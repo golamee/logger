d61146ba7a2e860703ab287ae56369266e308489
afrizaldm <afrizalmahendra212@gmail.com>
2025-03-12 16:52:42 +0700

    v1.0.0

--- log/log.go
+++ log/log.go
@@ -16,36 +16,39 @@ type Log struct {
 	Path string
 }
 
-func New() *Log {
-	log := &Log{
-		Path: "tmp/logs/",
+func New(Path ...string) *Log {
+
+	var log *Log
+
+	if len(Path) > 0 {
+		log = &Log{Path: Path[0]}
+	} else {
+		log = &Log{Path: "/tmp/logs/"}
 	}
 
-	log.Init()
+	return log.Init()
 
-	return log
 }
 
-func (l *Log) Init() {
+func (l *Log) Init() *Log {
 
 	// Buat folder log jika belum ada
 	if err := os.MkdirAll(l.Path, 0755); err != nil {
-		fmt.Println("Gagal membuat direktori log:", err)
+		fmt.Println("github.com/golamee/logger: Failed to create a log directory:", err)
 	}
+
+	return l
 }
 
 func (l *Log) write(title string, format string, v ...any) {
 
-	// Gunakan timezone dari perangkat atau default ke UTC+7 jika tidak ditemukan
 	timeNow := time.Now().Local()
 	date := timeNow.Format("2006-01-02")
 	logFileName := filepath.Join(l.Path, date+".log")
 
-	// Format log
 	timestamp := timeNow.Format("2006-01-02 15:04:05 MST")
 	logEntry := fmt.Sprintf("[%s] [%s] %s\n", timestamp, title, fmt.Sprintf(format, v...))
 
-	// Gunakan file dalam mode append
 	if file, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err == nil {
 		defer file.Close()
 		_, _ = file.WriteString(logEntry)
