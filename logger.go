d97c0fdfb41e34c4519d0e4257b9250d871c4e36
afrizaldm <afrizalmahendra212@gmail.com>
2025-03-13 12:33:58 +0700

    `Refactor logger package: replace NewLog with Init and add logging functions`

--- logger.go
+++ logger.go
@@ -2,6 +2,34 @@ package logger
 
 import "github.com/golamee/logger/log"
 
-func NewLog(Path ...string) {
-	log.New(Path...)
+var l *log.Log
+
+func Init(Path ...string) *log.Log {
+	l = log.New(Path...)
+
+	return l
+}
+
+func Info(Format string, v ...any) {
+	l.Info(Format, v...)
+}
+
+func Notice(Format string, v ...any) {
+	l.Notice(Format, v...)
+}
+
+func Alert(Format string, v ...any) {
+	l.Alert(Format, v...)
+}
+
+func Warning(Format string, v ...any) {
+	l.Warning(Format, v...)
+}
+
+func Error(Format string, v ...any) {
+	l.Error(Format, v...)
+}
+
+func Critical(Format string, v ...any) {
+	l.Critical(Format, v...)
 }
